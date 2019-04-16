0c0dfed91d47ea9cbf173b3380698fb85824596e
ahamacher <andrew.hamacher@gmail.com>
2019-04-16 15:03:57 -0700

    Add a lot of functions

--- jQuery Tic Tac Toe/src/index.js
+++ jQuery Tic Tac Toe/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js')
+const Game = require('./game.js')
 
   $(() => {
-    // Your code here
+    const $gameBoard = $('.ttt')
+    const game = new Game();
+    const view = new View(game, $gameBoard);
   });
--- jQuery Tic Tac Toe/src/ttt-view.js
+++ jQuery Tic Tac Toe/src/ttt-view.js
@@ -1,11 +1,42 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+    this.bindEvents();
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$el.on("click", "li", ( event => {
+      const $square = $(event.target);
+      this.makeMove($square);
+    }))
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    $square.removeClass('grey');
+    $square.addClass('white');
+    
+    $square.append(this.game.currentPlayer)
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    //create new html elements w/ jquery
+    const $grid = $('<ul>');
+    $grid.addClass('grid')
+    
+   
+    this.$el.append($grid);
+
+    for (let i = 0; i < 9; i++) {
+      let $li = $('<li>');
+      $grid.append($li);
+      $li.addClass('square')
+      $li.addClass('grey')
+    }
+
+  }
+  
 }
 
 module.exports = View;
